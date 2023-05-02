4c49af3305d4b376e7ac5c152cb3763037bd8b2e
veglos <veglos@laptop>
2023-05-01 21:32:33 -0400

    change to tokyonight

--- nvim/lua/veglos/plugins.lua
+++ nvim/lua/veglos/plugins.lua
@@ -48,7 +48,7 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
-
+  use 'folke/tokyonight.nvim'
   -- Fonts
   use "ryanoasis/vim-devicons"
 
