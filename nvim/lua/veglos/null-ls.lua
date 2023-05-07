f96b92034154a2f4b6aa5ab8028d835a41954ea7
veglos <veglos@laptop>
2023-05-06 22:32:23 -0400

    lsp rust

--- nvim/lua/veglos/null-ls.lua
+++ nvim/lua/veglos/null-ls.lua
@@ -5,5 +5,6 @@ null_ls.setup({
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.rustfmt,
   },
 })
