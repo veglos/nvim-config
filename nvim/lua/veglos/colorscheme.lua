278535dc64dd2bd5d60b8813bfa7eedea9420e4c
veglos <veglos@laptop>
2023-05-13 20:28:24 -0400

    adds tokyonight transparency

--- nvim/lua/veglos/colorscheme.lua
+++ nvim/lua/veglos/colorscheme.lua
@@ -1,8 +1,19 @@
----local colorscheme = "darkplus"
+--local colorscheme = "darkplus"
 local colorscheme = "tokyonight"
+
+if colorscheme == "tokyonight" then
+  require("tokyonight").setup {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    }
+  }
+end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
