18fba190127fef1806d37f8d37bd038b2fc25609
veglos <veglos@laptop>
2023-05-23 18:19:59 -0400

    add python formatter

--- nvim/lua/veglos/null-ls.lua
+++ nvim/lua/veglos/null-ls.lua
@@ -6,5 +6,6 @@ null_ls.setup({
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.formatting.autopep8,
   },
 })
