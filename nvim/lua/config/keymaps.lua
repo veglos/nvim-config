8d8cba925a4fc84f3cf7007bd78e0522cec1b760
veglos <veglos@laptop>
2023-11-26 23:08:24 -0300

    add tmux navigator

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -28,12 +28,21 @@ keymap("n", "x", '"_x', opts)
 keymap("n", "dd", '"_dd', opts)
 keymap("n", "dw", '"_dw', opts)
 
+-- clear search highlights
+keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Window management
+keymap("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
